b86d41a34471cf0900ba432273c991858019dee4
harri-renney-kaze <harri@kaze-consulting.com>
2024-11-07 18:09:19 +0000

    :bug: Fixed path to read elf header in benchmark

--- benchmark_output.sh
+++ benchmark_output.sh
@@ -2,70 +2,70 @@
 
 printf "\n\e[34mSTATIC NATIVE BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-native
+file bin/libsatcat-test-native
 printf "\e[37m"
 pmcstat -p CPU_CYCLES bin/libsatcat-test-native "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSTATIC HYBRID_ABI BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-hybridABI
+file bin/libsatcat-test-hybridABI
 printf "\e[37m"
 pmcstat -p CPU_CYCLES bin/libsatcat-test-hybridABI "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSTATIC PURECAP BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-purecap
+file bin/libsatcat-test-purecap
 printf "\e[37m"
 pmcstat -p CPU_CYCLES bin/libsatcat-test-purecap "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSTATIC BENCHMARK_ABI BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-benchmark
+file bin/libsatcat-test-benchmark
 printf "\e[37m"
 pmcstat -p CPU_CYCLES bin/libsatcat-test-benchmark "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSHARED LIBRARY NATIVE BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-native-dynamic-linked
+file bin/libsatcat-test-native-dynamic-linked
 printf "\e[37m"
 LD_64_LIBRARY_PATH=lib pmcstat -p CPU_CYCLES bin/libsatcat-test-native-dynamic-linked "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSHARED LIBRARY HybridABI BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-hybridABI-dynamic-linked
+file bin/libsatcat-test-hybridABI-dynamic-linked
 printf "\e[37m"
 LD_64_LIBRARY_PATH=lib pmcstat -p CPU_CYCLES bin/libsatcat-test-hybridABI-dynamic-linked "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSHARED LIBRARY PURECAP BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-purecap-dynamic-linked
+file bin/libsatcat-test-purecap-dynamic-linked
 printf "\e[37m"
 LD_LIBRARY_PATH=lib pmcstat -p CPU_CYCLES bin/libsatcat-test-purecap-dynamic-linked "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mSHARED LIBRARY BENCHMARK_ABI BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-hybridABI-dynamic-linked
+file bin/libsatcat-test-hybridABI-dynamic-linked
 printf "\e[37m"
 LD_64CB_LIBRARY_PATH=lib pmcstat -p CPU_CYCLES bin/libsatcat-test-benchmark-dynamic-linked "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mCOMPARTMENTALISED LIBRARY PURECAP BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-purecap-compartmentalised
+file bin/libsatcat-test-purecap-compartmentalised
 printf "\e[37m"
 LD_C18N_LIBRARY_PATH=lib pmcstat -p CPU_CYCLES bin/libsatcat-test-purecap-compartmentalised "test/test_data.txt"
 printf "\e[34m_______________________________\n"
 
 printf "\n\e[34mCOMPARTMENTALISED LIBRARY BENCHMARKABI BENCHMARK OUTPUT\n"
 printf "\e[35m"
-file ./libsatcat-test-benchmark-compartmentalised
+file bin/libsatcat-test-benchmark-compartmentalised
 printf "\e[37m"
 LD_64CB_C18N_LIBRARY_PATH=lib pmcstat -p CPU_CYCLES bin/libsatcat-test-benchmark-compartmentalised "test/test_data.txt"
 printf "\e[34m_______________________________\n"
